c701507a2403552f15b695415d83df4453decf36
Muppet237 <42935807+MemerRedeemer@users.noreply.github.com>
2021-11-01 18:47:27 +0100

    Static video working

--- Assets/Scripts/viewScript.cs
+++ Assets/Scripts/viewScript.cs
@@ -3,28 +3,34 @@ using UnityEngine.UI;
 
 public class viewScript:MonoBehaviour {
     [SerializeField] GameObject[] cameraArray;
+    [SerializeField] GameObject staticVideo;
     [SerializeField] Text cellNumberText;
 
+    float videoTimer = 0.5f;
     int selectedCamera = 0;
 
     void Start() {
-        changeCamera(0);
+
     }
 
     void Update() {
-        if(Input.GetKey(KeyCode.Alpha1)) {
-            selectedCamera = 0;
-            changeCamera(selectedCamera);
-        } else if(Input.GetKey(KeyCode.Alpha2)) {
-            selectedCamera = 1;
-            changeCamera(selectedCamera);
-        } else if(Input.GetKey(KeyCode.Alpha3)) {
-            selectedCamera = 2;
-            changeCamera(selectedCamera);
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            changeCamera(0);
+        } else if(Input.GetKeyDown(KeyCode.Alpha2)) {
+            changeCamera(1);
+        } else if(Input.GetKeyDown(KeyCode.Alpha3)) {
+            changeCamera(2);
+        } else if(Input.GetKeyDown(KeyCode.Alpha4)) {
+            changeCamera(3);
+        } else if(Input.GetKeyDown(KeyCode.Alpha5)) {
+            changeCamera(4);
+        } else if(Input.GetKeyDown(KeyCode.Alpha6)) {
+            changeCamera(5);
         }
     }
 
     public void changeCamera(int pressedCamera) {
+        staticVideo.SetActive(true);
         for(int i = 0; i < cameraArray.Length; i++) {
             if(i == pressedCamera) {
                 cameraArray[i].SetActive(true);
@@ -33,5 +39,10 @@ public class viewScript:MonoBehaviour {
                 cameraArray[i].SetActive(false);
             }
         }
+        Invoke("dissableStaticVideo", 0.3f);
+    }
+    
+    void dissableStaticVideo() {
+        staticVideo.SetActive(false);
     }
 }
