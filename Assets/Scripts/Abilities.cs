7892e8918f3aef8506416189e1cd2adb635fbb34
Lavisch <65831893+Lavisch@users.noreply.github.com>
2021-11-04 15:31:29 +0100

    added ability-bars

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -1,27 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Abilities : MonoBehaviour
 {
     [SerializeField] viewScript getCamViewNumber;
     [SerializeField] Prisoner[] prisonerScript;
+    [SerializeField] Slider electrocuteBar, gasBar;
 
     float electrocuteCooldown = 4;
     bool electrocuteReady = true;
+    int electrocuteUses = 10;
 
     float gasCooldown = 6;
     bool gasReady = true;
+    int gasUses = 5;
+
+    void Start()
+    {
+        electrocuteBar.maxValue = electrocuteUses;
+        electrocuteBar.value = electrocuteUses;
+
+        gasBar.maxValue = gasUses;
+        gasBar.value = gasUses;
+    }
 
     public void Electrocute()
     {
-        if (electrocuteReady)
+        if (electrocuteReady && electrocuteUses > 0)
         {
             prisonerScript[getCamViewNumber.selectedCamera].TakeDamage(20);
             prisonerScript[getCamViewNumber.selectedCamera].currentStates = Prisoner.State.electrocuted;
-            electrocuteReady = false;
             Invoke(nameof(ElectrocuteWait), electrocuteCooldown);
+
+            electrocuteUses -= 1;
+            electrocuteReady = false;
+            Debug.Log("Elec button works");
         }
+
+        electrocuteBar.value = electrocuteUses;
     }
 
     void ElectrocuteWait()
@@ -31,12 +49,16 @@ public class Abilities : MonoBehaviour
 
     public void Gas()
     {
-        if (gasReady)
+        if (gasReady && gasUses > 0)
         {
             prisonerScript[getCamViewNumber.selectedCamera].currentStates = Prisoner.State.gasedToSleep;
-            gasReady = false;
             Invoke(nameof(GasWait), gasCooldown);
+
+            gasUses -= 1;
+            gasReady = false;
+            Debug.Log("Gas button works");
         }
+        gasBar.value = gasUses;
     }
 
     void GasWait()
